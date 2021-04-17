d32ee74400134835d6fd2db7e118c6332e730910
Samuel Brehm <samuelbrehm@gmail.com>
2021-04-17 11:51:43 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Samuel Brehm on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
